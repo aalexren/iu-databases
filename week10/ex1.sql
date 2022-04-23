6e1a6360728b027413d84d53b0f3ee604be3f12a
Rodion <chernitca@ya.ru>
2022-04-23 16:05:56 +0300

    week10

--- week10/ex1.sql
+++ week10/ex1.sql
@@ -22,13 +22,37 @@ INSERT INTO account VALUES
 (2, 'Account 2', 1000, 'RUB'),
 (3, 'Account 3', 1000, 'RUB');
 
-BEGIN;
-SAVEPOINT T1;
-SELECT * transfer_money(1, 3, 500);
-SAVEPOINT T2;
-SELECT * transfer_money(2, 1, 700);
-SAVEPOINT T3;
-SELECT * transfer_money(2, 3, 100);
-ROLLBACK TO T1;
+ALTER TABLE account ADD COLUMN BankName VARCHAR;
+UPDATE account SET BankName = 'Sberbank' WHERE uid IN (1, 3);
+UPDATE account SET BankName = 'Tinkoff' WHERE uid = 2;
+
+INSERT INTO account VALUES (4, 'Account 4', 0, 'RUB', NULL);
+
+DROP FUNCTION IF EXISTS transfer_money_fee;
+CREATE OR REPLACE FUNCTION transfer_money_fee(from_uid INT, to_uid INT, amount DECIMAL)
+RETURNS TABLE 
+	(uid INT, name VARCHAR, credit DECIMAL, currency VARCHAR, bankname VARCHAR) AS 
+$$
+#variable_conflict use_column -- to not use uid_ in the arguments, 
+							  -- plpgsql will understand the difference between output
+							  -- and the input; details here:
+							  -- https://stackoverflow.com/questions/21662295/it-could-refer-to-either-a-pl-pgsql-variable-or-a-table-column
+	BEGIN
+		UPDATE account SET credit = credit + amount WHERE uid = to_uid;
+		UPDATE account SET credit = credit - amount WHERE uid = from_uid;
+
+		IF EXISTS (SELECT 1 FROM account AS A1, account AS A2 
+				   WHERE A1.uid = from_uid AND A2.uid = to_uid 
+				   AND A1.bankname != A2.bankname)
+		THEN
+			UPDATE account SET credit = credit - 30 WHERE uid = to_uid;
+			UPDATE account SET credit = credit + 30 WHERE uid = 4;
+		END IF;
+	
+		RETURN QUERY 
+			SELECT * FROM account;
+	END
+			
+$$ LANGUAGE plpgSQL;
+
 SELECT * FROM account;
-END;
